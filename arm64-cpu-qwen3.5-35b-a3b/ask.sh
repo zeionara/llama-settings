390eff3ff1bee9e17ced23e45c0f594321d14341
Zeio Nara <zeionara@gmail.com>
2026-03-04 23:49:42 +0300

    feat(model-configs): added more model configs

---
+++ arm64-cpu-qwen3.5-35b-a3b/ask.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+port=${2:-8000}
+host=${3:-localhost}
+
+if test -z "$1"; then
+  echo Question is required
+  exit 0
+fi
+
+curl http://$host:$port/v1/chat/completions \
+  -H 'Content-Type: application/json' \
+  -d "{\"model\": \"unsloth/Qwen3.5-35B-A3B-GGUF:Q4_K_M\", \"messages\": [{\"role\": \"user\", \"content\": \"$1\"}], \"stream\": false, \"stop\": [\"<|im_end|>\"]}" -s | python -m json.tool --no-ensure-ascii
